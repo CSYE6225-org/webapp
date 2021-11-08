bd8d223c757a17a68776027513a2e61487f294b8
maneesh2104 <maneesh.f@northeastern.edu>
2021-11-08 15:32:30 -0500

    scripts for CD

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/webapp/todo
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000 &
+python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2> /dev/null < /dev/null &

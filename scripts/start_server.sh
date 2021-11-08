52124af891fe70025fc2363353505d7d416b38e8
maneesh2104 <maneesh.f@northeastern.edu>
2021-11-08 15:52:34 -0500

    port change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/webapp/todo
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2> /dev/null < /dev/null &
+python3 manage.py runserver 0.0.0.0:5000 > /dev/null 2> /dev/null < /dev/null &

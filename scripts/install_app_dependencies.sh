6f90e5637816b28c2ba90d137f4be94243ac8bda
maneesh2104 <maneesh.f@northeastern.edu>
2021-11-14 21:59:32 -0500

    Loggers added

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/webapp/todo
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/ubuntu/webapp/todo/logs/watch.json\
+    -s
 pip install -r requirements.txt

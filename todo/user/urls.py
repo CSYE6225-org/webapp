3b8dbf03c293c603bd50731b167aeee95a5f6c95
maneesh2104 <maneesh.f@northeastern.edu>
2021-12-17 16:19:15 -0500

    Apponmnets api added

--- todo/user/urls.py
+++ todo/user/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('user/', views.Register.as_view()),
+    path('appoinment/', views.CreateApplication.as_view()),
     path('doctors/', views.DoctorsList.as_view()),
     path('user/self', views.GetUser.as_view()),
     path('verifyUserEmail', views.VerifyUser.as_view()),
